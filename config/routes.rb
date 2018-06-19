cba0969a0f97055a491951a2a429fca8b3f4f137
大野朱理 <ohnoakari@oonoakarinoMacBook-ea.local>
2018-06-19 13:44:08 +0900

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,6 @@ Rails.application.routes.draw do
   post 'posts/:id/destroy' => 'posts#destroy'
   get 'posts/:id' => 'posts#show'
 
-  get 'home/top' => 'home#top'
+  get '/' => 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
