217db75e047322bb861aa211cd0382139e8eaa3c
AkariOhno <akariohno22222@gmail.com>
2018-07-06 15:01:46 +0900

    toppage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
     def forbid_login_user
       if @current_user
         flash[:notice] = "すでにログインしています"
-        redirect_to("/post/index")
+        redirect_to("/posts/index")
       end
     end
 
